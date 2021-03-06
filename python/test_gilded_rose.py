d9cee260f47051eb12bb231a71c8fbd90de05c72
Slava Shpitalny <slavik57@gmail.com>
2021-03-06 17:48:53 +0200

    Add test for sulfuras

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,12 +4,19 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
+class Sulfuras(Item):
+    def __init__(self):
+        super(Sulfuras, self).__init__("Sulfuras, Hand of Ragnaros", sell_in=10, quality=50)
+    def __eq__(self, other):
+        return self.quality == other.quality and self.sell_in == other.sell_in
+
+
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_sulfuras_does_not_decrease_quality(self):
+        items = [Sulfuras()]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals(items[0], Sulfuras())
 
         
 if __name__ == '__main__':
