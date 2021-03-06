a0760855787b31ab86623ba26b590da73f80207f
Slava Shpitalny <slavik57@gmail.com>
2021-03-06 18:57:03 +0200

    refactor: extract update after sell in date

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -27,16 +27,20 @@ class GildedRose(object):
         if item.name != "Sulfuras, Hand of Ragnaros":
             item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
+            self.update_after_sell_in_date(item)
+
+    @staticmethod
+    def update_after_sell_in_date(item):
+        if item.name != "Aged Brie":
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+                item.quality = item.quality - item.quality
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
 
 
 class Item:
