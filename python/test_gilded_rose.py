983b30c8b114ee48e468771f5dff8df846054514
Slava Shpitalny <slavik57@gmail.com>
2021-03-06 17:52:54 +0200

    Add test for lowering item quality

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import GildedRose
+from gilded_rose import GildedRose, Item
 from items.sulfuras import Sulfuras
 
 
@@ -12,6 +12,12 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals(items[0], Sulfuras())
 
-        
+    def test_item_lowers_sell_in_and_quality(self):
+        items = [Item("Some item", sell_in=10, quality=4)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(items[0].sell_in, 9)
+        self.assertEquals(items[0].quality, 3)
+
 if __name__ == '__main__':
     unittest.main()
