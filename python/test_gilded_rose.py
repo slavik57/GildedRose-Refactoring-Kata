769b61e31e6ea3ff43a09aeda80853c17ea5bcdf
Slava Shpitalny <slavik57@gmail.com>
2021-03-06 18:01:09 +0200

    Non negative quality

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -9,8 +9,13 @@ class GildedRoseTest(unittest.TestCase):
     def setUp(self) -> None:
         self.item = Item("Some item", sell_in=10, quality=4)
         self.sulfuras = Sulfuras()
+        self.item_with_zero_quality = Item("Zero quality item", sell_in=10, quality=0)
 
-        self.items = [self.item, self.sulfuras]
+        self.items = [
+            self.item,
+            self.sulfuras,
+            self.item_with_zero_quality
+        ]
 
         gilded_rose = GildedRose(self.items)
         gilded_rose.update_quality()
@@ -21,6 +26,9 @@ class GildedRoseTest(unittest.TestCase):
     def test_item_lowers_sell_in_and_quality(self):
         self.assertEquals(self.item.sell_in, 9)
         self.assertEquals(self.item.quality, 3)
+
+    def test_quality_is_not_negative(self):
+        self.assertEquals(self.item_with_zero_quality.quality, 0)
 
 if __name__ == '__main__':
     unittest.main()
