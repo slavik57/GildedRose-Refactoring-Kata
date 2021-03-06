fe360f262be6384edd1bf1b2dded1f150893ae7a
Slava Shpitalny <slavik57@gmail.com>
2021-03-06 17:57:53 +0200

    Refactor tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,18 +6,21 @@ from items.sulfuras import Sulfuras
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_sulfuras_does_not_decrease_quality(self):
-        items = [Sulfuras()]
-        gilded_rose = GildedRose(items)
+    def setUp(self) -> None:
+        self.item = Item("Some item", sell_in=10, quality=4)
+        self.sulfuras = Sulfuras()
+
+        self.items = [self.item, self.sulfuras]
+
+        gilded_rose = GildedRose(self.items)
         gilded_rose.update_quality()
-        self.assertEquals(items[0], Sulfuras())
+
+    def test_sulfuras_does_not_decrease_quality(self):
+        self.assertEquals(self.sulfuras, Sulfuras())
 
     def test_item_lowers_sell_in_and_quality(self):
-        items = [Item("Some item", sell_in=10, quality=4)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals(items[0].sell_in, 9)
-        self.assertEquals(items[0].quality, 3)
+        self.assertEquals(self.item.sell_in, 9)
+        self.assertEquals(self.item.quality, 3)
 
 if __name__ == '__main__':
     unittest.main()
