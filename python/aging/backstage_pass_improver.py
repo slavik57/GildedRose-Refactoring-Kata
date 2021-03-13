9d82f8535afc4d4d6bf5b942a4400a205f011aef
Slava Shpitalny <slavik57@gmail.com>
2021-03-13 11:41:35 +0200

    Implement backstage pass in FP

--- python/aging/backstage_pass_improver.py
+++ python/aging/backstage_pass_improver.py
@@ -1,5 +1,7 @@
+from items.item import Item
 from .ager_types import OneDayAger
-from .item_improver import ItemImprover
+from .item_improver import ItemImprover, increase_quality_by
+from .item_aging import age_item
 
 
 class BackstagePassImprover(ItemImprover):
@@ -18,6 +20,22 @@ class BackstagePassImprover(ItemImprover):
         self.item.quality = 0
 
 
-def age_item_by_day(item) -> OneDayAger:
-    item_aging = BackstagePassImprover(item)
-    return lambda: item_aging.age_item_by_day()
+def age_item_by_day(item: Item) -> OneDayAger:
+    return lambda: age_item(
+        item=item,
+        before_sell_in=_update_quality_before_sell_in,
+        after_sell_in=_update_quality_after_sell_in
+    )
+
+
+def _update_quality_before_sell_in(item: Item) -> None:
+    if item.sell_in < 5:
+        increase_quality_by(item, 3)
+    elif item.sell_in < 10:
+        increase_quality_by(item, 2)
+    else:
+        increase_quality_by(item, 1)
+
+
+def _update_quality_after_sell_in(item: Item) -> None:
+    item.quality = 0
--- python/aging/item_improver.py
+++ python/aging/item_improver.py
@@ -29,12 +29,12 @@ def age_item_by_day(item) -> OneDayAger:
 
 
 def _update_quality_before_sell_in(item: Item) -> None:
-    _increase_quality_by(item, 1)
+    increase_quality_by(item, 1)
 
 
 def _update_quality_after_sell_in(item: Item) -> None:
-    _increase_quality_by(item, 2)
+    increase_quality_by(item, 2)
 
 
-def _increase_quality_by(item: Item, quality_to_add: int) -> None:
+def increase_quality_by(item: Item, quality_to_add: int) -> None:
     item.quality = min(item.quality + quality_to_add, MAX_ITEM_QUALITY)
