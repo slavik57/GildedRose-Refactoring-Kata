410b555a396c5fc7c35f622905c595cf8cf48d09
Slava Shpitalny <slavik57@gmail.com>
2021-03-13 11:53:37 +0200

    age item defaults

--- python/aging/item_aging.py
+++ python/aging/item_aging.py
@@ -28,29 +28,11 @@ class ItemAging:
 
 
 def age_item_by_day(item: Item) -> OneDayAger:
-    return lambda: age_item(
-        item=item,
-        before_sell_in=_update_quality_before_sell_in,
-        after_sell_in=_update_quality_after_sell_in
-    )
+    return lambda: age_item(item=item)
 
 
 def _update_sell_in(item: Item) -> None:
     item.sell_in = item.sell_in - 1
-
-
-def age_item(
-        item: Item,
-        before_sell_in: ItemUpdater,
-        after_sell_in: ItemUpdater,
-        update_sell_in: ItemUpdater = _update_sell_in
-):
-    update_sell_in(item)
-
-    if item.sell_in < 0:
-        after_sell_in(item)
-    else:
-        before_sell_in(item)
 
 
 def _update_quality_before_sell_in(item: Item) -> None:
@@ -59,6 +41,18 @@ def _update_quality_before_sell_in(item: Item) -> None:
 
 def _update_quality_after_sell_in(item: Item) -> None:
     reduce_quality_by(item, 2)
+
+
+def age_item(item: Item,
+             before_sell_in: ItemUpdater = _update_quality_before_sell_in,
+             after_sell_in: ItemUpdater = _update_quality_after_sell_in,
+             update_sell_in: ItemUpdater = _update_sell_in):
+    update_sell_in(item)
+
+    if item.sell_in < 0:
+        after_sell_in(item)
+    else:
+        before_sell_in(item)
 
 
 def reduce_quality_by(item: Item, amount: int) -> None:
