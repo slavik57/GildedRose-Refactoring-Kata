6a7053f6d77c415cea524a994c6a1ec2fa4ccde3
Slava Shpitalny <slavik57@gmail.com>
2021-03-13 11:09:26 +0200

    Make aging export from a module

---
+++ python/aging/__init__.py
@@ -0,0 +1,5 @@
+from .conjured_aging import age_item_by_day as age_conjured_by_day
+from .item_aging import age_item_by_day
+from .backstage_pass_improver import age_item_by_day as age_backstage_pass_by_day
+from .sulfuras_aging import age_item_by_day as age_sulfuras_by_day
+from .ager_types import *
